08900f5e0d30d0875c3d0b7987e55e896a21bc0b
dingziyang <565444994@qq.com>
2017-11-01 19:30:04 +0800

    2-3学完

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
+//从ng的核心模块angular/core里引入了一个叫Component的装饰器
 import { Component } from '@angular/core';
 
+/**
+ * AppComponent是一个普通的typescript类，通过装饰器附加元数据到typescript类上面
+ * ng就知道了，要把它变成一个ng组件
+ * 
+ * selector：告诉这个组件可以通过app-root'这样一个标签来调用
+ * templateUrl： 指定一个html文件作为组件模板
+ * styleUrls：指向这个组件模板需要使用到的样式
+ */
+/**
+ * export：定义了这个组件的控制器，它只有一个属性，叫title
+ */
+//用这个装饰器定义了一个组件，以及组件的元数据
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+/**
+ * 模块
+ * @NgModule 声明了一个模块
+ * declarations： 声明模块中有什么东西（只能声明组件，指令，管道）
+ * imports ：声明模块中依赖的其他模块
+ * providers： 声明模块中提供什么服务（只能声明服务）
+ * bootstrap： 声明模块的 [主] 组件
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
